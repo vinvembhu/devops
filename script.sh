f0c694af3560fd85769978a62f9ce5d206b5a397
vinvembhu <112843120+vinvembhu@users.noreply.github.com>
2022-09-10 12:21:27 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -x
 docker login -u $DOCKER_USER -p $DOCKER_PASSWORD jenkinscicd.azurecr.io
 docker run -id --name hello-world $DOCKER_IMAGE_NAME:$DOCKER_IMAGE_TAG /bin/bash
 docker ps | grep -i hello-world
