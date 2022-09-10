d07026a87fb641763fefa706a565414cf84136f7
vinvembhu <112843120+vinvembhu@users.noreply.github.com>
2022-09-10 12:02:46 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-echo $DOCKER_IMAGE
 docker login -u $DOCKER_USER -p $DOCKER_PASSWORD jenkinscicd.azurecr.io
-docker run -id --name hello-world $DOCKER_IMAGE /bin/bash
+docker run -id --name hello-world $DOCKER_IMAGE_NAME:$DOCKER_IMAGE_TAG /bin/bash
 docker ps | grep -i hello-world
 if [ $? -eq 0 ] ;then
   echo "container created"
